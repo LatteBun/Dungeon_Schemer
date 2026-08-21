4e94c730d202dd576240f899b1de3c682bfc23c1
SangHwan Yoo <31851508+QuaintCoding@users.noreply.github.com>
2026-08-21 23:49:44 +0900

    화면: 사막 몬스터 검수 라우트를 추가한다
    개발용 /desert-monsters-test 경로에서 반응형 에셋 카탈로그를 렌더링한다.

---
+++ app/desert-monsters-test/page.tsx
@@ -0,0 +1,8 @@
+import { DesertMonsterCatalog } from "@/components/game/DesertMonsterCatalog";
+import "./desert-monsters.css";
+
+export function DesertMonstersTestPage() {
+  return <DesertMonsterCatalog />;
+}
+
+export default DesertMonstersTestPage;
