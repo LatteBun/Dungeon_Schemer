20e734b14c428e86d083ab32b1f7c1a5b87eb0d2
SangHwan Yoo <31851508+QuaintCoding@users.noreply.github.com>
2026-08-22 16:27:46 +0900

    feat: load U4 dungeon map styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ import "./u3-card-theme.css";
 import "./u3-large-screen.css";
 import "./u3-contract-layout.css";
 import "./u3-responsive-layout.css";
+import "./u4-dungeon-map.css";
 
 export const metadata: Metadata = {
   title: "Dungeon Schemer",
